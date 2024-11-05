4c59893173d9053429557a7529ada3629804ba81
jiban <aijiban.nbr@gmail.com>
2024-11-05 16:13:27 +0600

    Edit commit: using userInput with .gitignore and README

--- src/PracticeGitIgnore.java
+++ src/PracticeGitIgnore.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class PracticeGitIgnore {
     public static void main(String[] args) {
 
-        String s1 = "Hello, ";
-        String s2 = "GitIgnore Task3!";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1st String:");
+        String s1 = scanner.nextLine();
+        System.out.println("Enter 2nd String:");
+        String s2 = scanner.nextLine();
         System.out.println(s1.concat(s2));
+        scanner.close();
     }
 }
