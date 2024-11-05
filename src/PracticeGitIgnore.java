92da4bbe8bb0afc273af1f09276f57399d024e83
jiban <aijiban.nbr@gmail.com>
2024-11-05 15:46:28 +0600

    Initial commit: Add PracticeGitIgnore project with .gitignore and README

---
+++ src/PracticeGitIgnore.java
@@ -0,0 +1,8 @@
+public class PracticeGitIgnore {
+    public static void main(String[] args) {
+
+        String s1 = "Hello, ";
+        String s2 = "GitIgnore Task3!";
+        System.out.println(s1.concat(s2));
+    }
+}
